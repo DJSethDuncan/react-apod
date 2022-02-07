db470f89d9416d209277569e9813f9249ccc6264
Seth Duncan <sethduncan@gmail.com>
2022-02-07 13:40:20 -0600

    update .env situation

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,8 @@ class App extends React.Component<AppProps, AppState> {
   }
 
   async componentDidMount() {
-    const apiKey = "";
+    const apiKey = process.env.REACT_APP_NASA_API_TOKEN;
+    console.log("env ->", process.env);
     const result = await axios.get(
       `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
     );
