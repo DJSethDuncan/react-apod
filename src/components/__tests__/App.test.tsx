27e6a314de243e369a00633153158ebf1c8075af
Seth Duncan <sethduncan@gmail.com>
2023-12-29 00:39:15 -0600

    fix test

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -4,7 +4,7 @@ import App from "../App";
 describe("App", () => {
   it("renders", () => {
     render(<App />);
-    const app = screen.getByText("APOD");
+    const app = screen.getByText("NASA Astronomy Photo of the Day");
     expect(app).toBeVisible();
   });
 });
